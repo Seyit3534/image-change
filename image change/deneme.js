55bfb6d129fcacd469085b53873f2533e8bb672d
Seyit3534 <140590613+Seyit3534@users.noreply.github.com>
2023-10-01 14:58:42 +0300

    Add files via upload

---
+++ image change/deneme.js
@@ -0,0 +1,19 @@
+
+const resim1 = () => {
+    document.getElementById("bir").style.display ='block';
+    document.getElementById("iki").style.display ='none'
+    document.getElementById("uc").style.display ='none'
+  }
+  
+  const resim2 = () => {
+    document.getElementById("iki").style.display ='block'
+    document.getElementById("bir").style.display ='none'
+    document.getElementById("uc").style.display ='none'
+  }
+  
+  const resim3 = () => {
+    document.getElementById("uc").style.display ='block'
+    document.getElementById("bir").style.display ='none'
+    document.getElementById("iki").style.display ='none'
+  }
+  
